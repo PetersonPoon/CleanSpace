9895f07642d52e478772d68c6166d83edfdb5318
echiang23 <elaine.y.chiang@gmail.com>
2014-11-27 18:58:41 -0800

    Service

--- src/com/example/cleanspace/MainActivity.java
+++ src/com/example/cleanspace/MainActivity.java
@@ -12,11 +12,8 @@ import android.content.Intent;
 import android.content.ServiceConnection;
 import android.graphics.Color;
 import android.os.Bundle;
-import android.os.Handler;
 import android.os.IBinder;
-import android.os.Message;
 import android.os.Messenger;
-import android.os.RemoteException;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -24,8 +21,6 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.LinearLayout;
-import android.widget.TextView;
-import android.widget.Toast;
 
 /**
  * This activity will display the general details of our one sensor Details
@@ -41,50 +36,49 @@ public class MainActivity extends Activity {
 	Messenger mService = null;
 	boolean mIsBound;
 
-	// LocalService mBoundService;
-	//
-	// private ServiceConnection mConnection = new ServiceConnection() {
-	// public void onServiceConnected(ComponentName className, IBinder service)
-	// {
-	// mBoundService = ((LocalService.LocalBinder) service).getService();
-	// }
-	//
-	// public void onServiceDisconnected(ComponentName className) {
-	// // This is called when the connection with the service has been
-	// // unexpectedly disconnected -- that is, its process crashed.
-	// // Because it is running in our same process, we should never
-	// // see this happen.
-	// mBoundService = null;
-	// }
-	// };
-	//
-	// void doBindService() {
-	// // Can we somehow pass the sensorFileTitle to the service?
-	// bindService(new Intent(this, LocalService.class), mConnection,
-	// Context.BIND_AUTO_CREATE);
-	// mIsBound = true;
-	// }
-	//
-	// void doUnbindService() {
-	// if (mIsBound) {
-	// // If we have received the service, and hence registered with it,
-	// // then now is the time to unregister.
-	//
-	// // Detach our existing connection.
-	// unbindService(mConnection);
-	// mIsBound = false;
-	// }
-	// }
-	//
-	// @Override
-	// protected void onDestroy() {
-	// super.onDestroy();
-	// try {
-	// doUnbindService();
-	// } catch (Throwable t) {
-	// Log.e("MainActivity", "Failed to unbind from the service", t);
-	// }
-	// }
+	LocalService mBoundService;
+
+	private ServiceConnection mConnection = new ServiceConnection() {
+		public void onServiceConnected(ComponentName className, IBinder service) {
+			mBoundService = ((LocalService.LocalBinder) service).getService();
+		}
+
+		public void onServiceDisconnected(ComponentName className) {
+			// This is called when the connection with the service has been
+			// unexpectedly disconnected -- that is, its process crashed.
+			// Because it is running in our same process, we should never
+			// see this happen.
+			mBoundService = null;
+		}
+	};
+
+	void doBindService() {
+		// Can we somehow pass the sensorFileTitle to the service?
+		bindService(new Intent(this, LocalService.class), mConnection,
+				Context.BIND_AUTO_CREATE);
+		mIsBound = true;
+	}
+
+	void doUnbindService() {
+		if (mIsBound) {
+			// If we have received the service, and hence registered with it,
+			// then now is the time to unregister.
+
+			// Detach our existing connection.
+			unbindService(mConnection);
+			mIsBound = false;
+		}
+	}
+
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+		try {
+			doUnbindService();
+		} catch (Throwable t) {
+			Log.e("MainActivity", "Failed to unbind from the service", t);
+		}
+	}
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -95,13 +89,13 @@ public class MainActivity extends Activity {
 
 		// Start background service
 		// use this to start and trigger a service
-		// Intent startServiceIntent = new Intent(context, LocalService.class);
+		Intent startServiceIntent = new Intent(context, LocalService.class);
 		// potentially add data to the intent
-		// context.startService(startServiceIntent);
+		context.startService(startServiceIntent);
 
 		// TODO
 		// Binds to get data, but how do we set how often it binds?
-		// doBindService();
+		doBindService();
 	}
 
 	private class newClick implements OnClickListener {
